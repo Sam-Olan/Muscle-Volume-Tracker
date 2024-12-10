1eeb04c7608cf0605ee0de8881296ad76082a66a
Sam-Olan <98357385+Sam-Olan@users.noreply.github.com>
2024-12-10 11:43:29 -0500

    Initial Commit

---
+++ Muscle Volume Tracker/Muscle_Volume_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Muscle_Volume_TrackerApp.swift
+//  Muscle Volume Tracker
+//
+//  Created by Sam Olan on 2024-12-10.
+//
+
+import SwiftUI
+
+@main
+struct Muscle_Volume_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
